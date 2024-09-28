c17d26d062f4326a43214e8aea7c43775a11e3f4
alex241728 <alex_241728@163.com>
2024-09-28 11:08:51 -0400

    revise 7

---
+++ 7 Reverse Integer/Java/Driver.java
@@ -0,0 +1,23 @@
+public class Driver {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        /*
+         * Input: x = 123
+         * Output: 321
+         */
+        System.out.println(s.reverse(123));
+
+        /*
+         * Input: x = -123
+         * Output: -321
+         */
+        System.out.println(s.reverse(-123));
+
+        /*
+         * Input: x = 120
+         * Output: 21
+         */
+        System.out.println(s.reverse(120));
+    }
+}
--- 7 Reverse Integer/Java/Solution.java
+++ 7 Reverse Integer/Java/Solution.java
@@ -1,9 +1,10 @@
-public class ReverseInteger {
+public class Solution {
     public int reverse(int x) {
         int res = 0;
 
         while (x != 0) {
             int digit = x % 10;
+
             if (x > 0 && res > (Integer.MAX_VALUE - digit) / 10) {
                 return 0;
             }
@@ -12,9 +13,9 @@ public class ReverseInteger {
             }
             
             res = res * 10 + digit;
+
             x /= 10;
         }
-
         return res;
     }
 }
