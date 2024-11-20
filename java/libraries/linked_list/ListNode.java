11bba7b7bc1950838a367effca60b4e9e6650f1a
alex241728 <alex_241728@163.com>
2024-11-20 11:59:02 -0500

    141

--- java/libraries/linked_list/ListNode.java
+++ java/libraries/linked_list/ListNode.java
@@ -1,11 +1,16 @@
 package libraries.linked_list;
 
 public class ListNode {
-    public int val;
-    public ListNode next;
+  public int val;
+  public ListNode next;
 
-    public ListNode(int val) {
+  public ListNode(int val) {
+    this.val = val;
+    this.next = null;
+  }
+
+    public ListNode(int val, ListNode next) {
         this.val = val;
-        this.next = null;
+        this.next = next;
     }
 }
---
+++ java/linked_list_cycle_141/Solution.java
@@ -0,0 +1,23 @@
+package linked_list_cycle_141;
+
+import libraries.linked_list.ListNode;
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
