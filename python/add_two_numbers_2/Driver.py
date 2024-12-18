787dea9406a18e0356edb007853be5c346422355
alex241728 <alex_241728@163.com>
2024-12-18 13:05:05 -0500

    2

---
+++ python/add_two_numbers_2/Driver.py
@@ -0,0 +1,37 @@
+from add_two_numbers_2.Solution import Solution
+from tree.linked_list.ListNode import ListNode
+
+
+class Driver:
+    if __name__ == "__main__":
+        s: Solution = Solution()
+
+        """
+            Input: l1 = [2,4,3], l2 = [5,6,4]
+            Output: [7,0,8]
+            Explanation: 342 + 465 = 807.
+        """
+        l1: ListNode = ListNode(2, ListNode(4, ListNode(3)))
+        l2: ListNode = ListNode(5, ListNode(6, ListNode(4)))
+        ListNode.print(s.addTwoNumbers(l1, l2))
+
+        """
+            Input: l1 = [0], l2 = [0]
+            Output: [0]
+        """
+        l1: ListNode = ListNode(0)
+        l2: ListNode = ListNode(0)
+        ListNode.print(s.addTwoNumbers(l1, l2))
+
+        """
+            Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+            Output: [8,9,9,9,0,0,0,1]
+        """
+        l1 = ListNode(
+            9,
+            ListNode(
+                9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9)))))
+            ),
+        )
+        l2 = ListNode(9, ListNode(9, ListNode(9, ListNode(9))))
+        ListNode.print(s.addTwoNumbers(l1, l2))
---
+++ python/add_two_numbers_2/Solution.py
@@ -0,0 +1,31 @@
+from typing import Optional, Self
+
+from tree.linked_list.ListNode import ListNode
+
+
+class Solution:
+    def addTwoNumbers(
+        self: Self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy: ListNode = ListNode(-1)
+        current: ListNode = dummy
+
+        carry: int = 0
+
+        while l1 or l2:
+            num: int = carry
+            if l1:
+                num += l1.val
+                l1 = l1.next
+            if l2:
+                num += l2.val
+                l2 = l2.next
+            digit: int = num % 10
+            carry = num // 10
+            current.next = ListNode(digit)
+            current = current.next
+
+        if carry:
+            current.next = ListNode(carry)
+
+        return dummy.next
